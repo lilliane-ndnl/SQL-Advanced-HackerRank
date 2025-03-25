79166e2af1a47e8f255829ce2be26d6ebef301df
Lilliane <135808754+lilliane-ndnl@users.noreply.github.com>
2025-03-25 20:58:03 +0700

    Update solution.sql

--- Question-1-Weather-Analysis/solution.sql
+++ Question-1-Weather-Analysis/solution.sql
@@ -1,23 +1,13 @@
--- Solution to calculate weekend hours worked per employee
-WITH weekend_data AS (
-    SELECT 
-        emp_id, 
-        STR_TO_DATE(`timestamp`, '%Y-%m-%d %H:%i:%s') AS datetime,
-        ROW_NUMBER() OVER (PARTITION BY emp_id ORDER BY STR_TO_DATE(`timestamp`, '%Y-%m-%d %H:%i:%s')) AS rn
-    FROM attendance
-    WHERE WEEKDAY(STR_TO_DATE(`timestamp`, '%Y-%m-%d %H:%i:%s')) IN (5, 6) -- Saturday (5) & Sunday (6)
-),
-paired_data AS (
-    SELECT 
-        w1.emp_id,
-        TIMESTAMPDIFF(HOUR, w1.datetime, w2.datetime) AS hours_worked
-    FROM weekend_data w1
-    JOIN weekend_data w2 
-        ON w1.emp_id = w2.emp_id 
-        AND w1.rn % 2 = 1 -- Take only odd rows as IN times
-        AND w2.rn = w1.rn + 1 -- Match with the next timestamp (OUT time)
-)
-SELECT emp_id, SUM(hours_worked) AS work_hours
-FROM paired_data
-GROUP BY emp_id
-ORDER BY work_hours DESC;
+/*
+Enter your query below.
+Please append a semicolon ";" at the end of the query.
+*/
+
+SELECT 
+    MONTH(STR_TO_DATE(record_date, '%Y-%m-%d')) AS month,
+    MAX(CASE WHEN data_type = 'max' THEN data_value END) AS max,
+    MIN(CASE WHEN data_type = 'min' THEN data_value END) AS min,
+    ROUND(AVG(CASE WHEN data_type = 'avg' THEN data_value END)) AS avg
+FROM temperature_records
+GROUP BY MONTH(STR_TO_DATE(record_date, '%Y-%m-%d'))
+ORDER BY month;
